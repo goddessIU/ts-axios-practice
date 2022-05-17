315ababc81ee2e4999c855a573b234f3d0d2613b
goddessIU <2581606394@qq.com>
2022-05-18 00:06:20 +0800

    feat<*> 基础功能

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
+
+export type Method = 'get' | 'GET'
+  | 'delete' | 'DELETE'
+  | 'post' | 'POST'
+  | 'head' | 'HEAD'
+  | 'options' | 'OPTIONS'
+  | 'put' | 'PUT'
+  | 'patch' | 'PATCH'
